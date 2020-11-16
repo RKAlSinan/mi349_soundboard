278005716ed6036c750f7ca7a83036736f0ca9ab
Reem AlSinan <70866569+RKAlSinan@users.noreply.github.com>
2020-11-15 23:26:54 -0500

    Add files via upload

---
+++ soundboard.js
@@ -0,0 +1,8 @@
+
+
+var bleep = new Audio ();
+bloop_x.src = "bloop_x.wav";
+function loadContent (num) { 
+    bloop_x.play();
+    var div1=document.getElementById("div1");
+    div1.innerHTML ="Loaded content for section"+num;
